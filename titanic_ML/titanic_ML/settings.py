ee5fd017adeb72ebb842279dd75ea675475c9c17
Shre9yansh <89804452+Shre9ansh@users.noreply.github.com>
2022-07-06 16:01:47 +0530

    Update settings.py

--- titanic_ML/titanic_ML/settings.py
+++ titanic_ML/titanic_ML/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-38@9u_9w4n5%bv$8o9$stm*zo&^!=-bs#!el(qj0xv@v3*pi&1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.109.54.171']
 
 
 # Application definition
